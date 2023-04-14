7dc963772173e8cea377cf323225b5cde0436195
Ian7140 <rlawnals7140@gmail.com>
2023-04-14 23:41:16 +0900

    list

--- project.py
+++ project.py
@@ -1,31 +1,4 @@
-playing = True
-
-a = int(input("Choose a number:\n"))
-b = int(input("Choose another one:\n"))
-operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
-  
-# add your code here!
-
-while playing:
-    if operation == '*':
-        print("Result:", a*b)
-        a = int(input("Choose a number:\n"))
-        b = int(input("Choose another one:\n"))
-        operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
-    elif operation == '+':
-        print("Result:", a+b)
-        a = int(input("Choose a number:\n"))
-        b = int(input("Choose another one:\n"))
-        operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
-    elif operation == '-':
-        print("Result:", a-b)
-        a = int(input("Choose a number:\n"))
-        b = int(input("Choose another one:\n"))
-        operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
-    elif operation == '/':
-        print("Result:", a/b)
-        a = int(input("Choose a number:\n"))
-        b = int(input("Choose another one:\n"))
-        operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
-    elif operation == 'exit':
-        playing = False
+#list : c언어에서의 배열과 비슷한 개념
+days_of_week = ["Mon","Tue","Wed","Thu","Fri"]
+name = "nico"
+print(name.upper())
