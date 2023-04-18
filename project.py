ce01c51c52a33da2dedf3f83a9e1cd784be2268e
Ian7140 <rlawnals7140@gmail.com>
2023-04-19 00:51:32 +0900

    results dictionary

--- project.py
+++ project.py
@@ -8,9 +8,16 @@ websites = (
     "https://tiktok.com"
 )
 
+results = {}
+
 for each in websites: 
     #for 뒤에 오는 건 placeholder , 보통 movie in movies, website in websites 의 형식으로 만듦
     if not each.startswith("https://"):
         each = f"https://{each}"
     response = get(each) #띄어쓰기 중요함
-    print(response)
+    if response.status_code == 200:
+      results[each] = "OK"
+    else:
+      results[each] = "FAILED"
+
+print(results)
